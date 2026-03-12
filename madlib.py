53f6ac1f94ec26abc1c19fd342e8cca944ba0efa
ndongopamsy08-hue <ndongopamsy08@gmail.com>
2026-03-12 14:10:25 +0000

    Add python fundamentals projec and exercises

---
+++ madlib.py
@@ -0,0 +1,7 @@
+answer1=input("answer queston1: ")
+answer2=input("answer queston2: ")
+answer3=input("answer queston3: ")
+print(f"the interconnection of computer which are connected with the main aim of charing resources is {answer1}\n\n")
+print(f"give me one things that a good network should have to be good {answer2}\n\n")
+
+print(f"the connection of computer withing a very very short rangd is called{answer3}\n\n")
